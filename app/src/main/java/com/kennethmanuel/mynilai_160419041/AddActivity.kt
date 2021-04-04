fed0a59113eee0ac4396d902eb1c87ee117b51a2
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-04-04 19:14:29 +0700

    UI

---
+++ app/src/main/java/com/kennethmanuel/mynilai_160419041/AddActivity.kt
@@ -0,0 +1,11 @@
+package com.kennethmanuel.mynilai_160419041
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
---
+++ app/src/main/java/com/kennethmanuel/mynilai_160419041/CardActivity.kt
@@ -0,0 +1,11 @@
+package com.kennethmanuel.mynilai_160419041
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card)
+    }
+}
